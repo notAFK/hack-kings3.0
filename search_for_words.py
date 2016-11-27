ee7602e2d2a2f6480a4ee7cb317939cb9612dc7f
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2016-11-27 01:29:54 +0000

    Examples of streaming and searching for tweets

---
+++ search_for_words.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tweepy
+import json
+import config
+
+#authorize twitter, initialize tweepy
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+#refer http://docs.tweepy.org/en/v3.2.0/api.html#API
+#tells tweepy.API to automatically wait for rate limits to replenish
+
+#Put your search term
+searchquery = "love"
+
+'''
+Source of the template along with query operators
+http://nocodewebscraping.com/twitter-json-examples/
+'''
+
+users =tweepy.Cursor(api.search,q=searchquery).items()
+count = 0
+errorCount=0
+
+file = open('search_for_words.json', 'wb') 
+
+while True:
+    try:
+        user = next(users)
+        #count is the number of tweets
+        count += 1
+        #use count-break during dev to avoid twitter restrictions
+        if (count>10):
+           break
+    except tweepy.TweepError:
+        #catches TweepError when rate limiting occurs, sleeps, then restarts.
+        #nominally 15 minnutes, make a bit longer to avoid attention.
+        print "sleeping...."
+        time.sleep(60*16)
+        user = next(users)
+    except StopIteration:
+        break
+    try:
+        print "Writing to JSON tweet number:"+str(count)
+        json.dump(user._json,file,sort_keys = True,indent = 4)
+        
+    except UnicodeEncodeError:
+        errorCount += 1
+        print "UnicodeEncodeError,errorCount ="+str(errorCount)
+
+print "completed, errorCount ="+str(errorCount)+" total tweets="+str(count)
+    
+    #todo: write users to file, search users for interests, locations etc.
+
+"""
+http://docs.tweepy.org/en/v3.5.0/api.html?highlight=tweeperror#TweepError
+NB: RateLimitError inherits TweepError.
+http://docs.tweepy.org/en/v3.2.0/api.html#API  wait_on_rate_limit & wait_on_rate_limit_notify
+NB: possibly makes the sleep redundant but leave until verified.
+"""
---
+++ streaming_new_tweets.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import tweepy
+import json
+import config
+
+# This is the listener, resposible for receiving data
+class StdOutListener(tweepy.StreamListener):
+    def on_data(self, data):
+        # Parsing 
+        decoded = json.loads(data)
+        #open a file to store the status objects
+        file = open('streaming_new_tweets.json', 'wb')  
+        #write json to file
+        json.dump(decoded,file,sort_keys = True,indent = 4)
+        #show progress
+        print "Writing tweets to file,CTRL+C to terminate the program"
+
+        
+        return True
+
+    def on_error(self, status):
+        print status
+
+if __name__ == '__main__':
+    l = StdOutListener()
+    # auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    # auth.set_access_token(config.access_token, config.access_token_secret)
+
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token, config.access_secret)
+
+    # There are different kinds of streams: public stream, user stream, multi-user streams
+    # For more details refer to https://dev.twitter.com/docs/streaming-apis
+    stream = tweepy.Stream(auth, l)
+
+    #Hashtag to stream
+    stream.filter(track=["#love"])  #Replace with your favorite hashtag or query
