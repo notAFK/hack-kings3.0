063d28c4b8053fe9a257def19905559510c2d570
Alex Radu <dragosthealx@gmail.com>
2016-11-27 00:13:22 +0000

    Added more stuff in util. Added read tweets, filter tweet methods

--- analyser.py
+++ analyser.py
@@ -1,3 +1,26 @@
 '''This takes all the tweets from a sqlite3 db in their text form and performs
 filtering and sentiment analysis on them, returning the features into another
 sqlite3 db'''
+
+import utils
+
+def init(company = None):
+    # TODO: make this get the company from scraper
+    COMPANY = TEST_PARAMS['ANALYSER_COMPANY']
+
+def read_tweets():
+    conn, c = get_database_connection(DATABASES['STUB_RAW_TWEETS_DB'])
+    c.execute('''SELECT * FROM ? LIMIT 10000''', (COMPANY,))
+    return c
+
+def filter_tweet(tweet):
+    return tweet
+
+def get_filtered_tweets():
+    # Get all tweets from db
+    c = read_tweets()
+    # Filter them
+    tweets = []
+    for tweet in c.fetchall():
+        tweets.append((tweet[0], filter_tweet(tweet[1])))
+    return tweets
--- utils.py
+++ utils.py
@@ -4,10 +4,13 @@ global vars'''
 import sqlite3
 import os
 
-__init__ = ['DATABASES', 'get_database_connection']
+__init__ = ['DATABASES', 'get_database_connection', 'TEST_PARAMS']
 
+TEST_PARAMS = {'ANALYSER_COMPANY',
+               }
 DATABASES = {'RAW_TWEETS_DB': 'raw_tweets.db',
-             'FEATURES_DB'  : 'extracted_features.db'
+             'FEATURES_DB'  : 'extracted_features.db',
+             'STUB_RAW_TWEETS_DB': 'stub_raw_tweets.db',
             }
 
 def get_database_connection(db_path):
