457670306a60d2bd010e053d432d2fafbb68872d
Alex Radu <dragosthealx@gmail.com>
2016-11-27 02:57:51 +0000

    Added get_ticker() function to get a company ticker symbol from a company name

--- utils.py
+++ utils.py
@@ -3,11 +3,13 @@ global vars'''
 
 import sqlite3
 import os
+import json
+import requests
 
 __init__ = ['DATABASES', 'get_database_connection', 'TEST_PARAMS', 'URLS',
             'VERBOSE', 'create_table']
 
-URLS = {}
+URLS = {'COMPANY_NAME_TO_TICKER_API': 'http://chstocksearch.herokuapp.com/api/'}
 VERBOSE = True
 TEST_PARAMS = {'ANALYSER_COMPANY': 'starbucks'}
 DATABASES = {'RAW_TWEETS_DB': 'raw_tweets.db',
@@ -25,3 +27,8 @@ def create_tweets_table(db_path, table_name):
               ''' (hash text, tweet text, ts timestamp)''')
     conn.commit()
     conn.close()
+
+def get_ticker(company):
+    response = requests.get(URLS['COMPANY_NAME_TO_TICKER_API'] + company)
+    response = json.loads(response.content)
+    return (response[0])['symbol']
