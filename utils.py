761e77731963ca28d2656372b349ae4c2b425b43
Paul Chelarescu <paul-constantin.chelarescu@student.manchester.ac.uk>
2016-11-27 04:16:11 +0000

    Added dictionaries for few companies

--- utils.py
+++ utils.py
@@ -11,8 +11,52 @@ __init__ = ['DATABASES', 'get_database_connection', 'TEST_PARAMS', 'URLS',
 
 URLS = {'COMPANY_NAME_TO_TICKER_API': 'http://chstocksearch.herokuapp.com/api/'}
 VERBOSE = True
-TEST_PARAMS = {'ANALYSER_COMPANY': 'starbucks',
+TEST_PARAMS = {'ANALYSER_COMPANY': 'starbucks OR '
+                                    + 'coffee OR '
+                                    + 'medusa' ,
                 'MAX_TWEETS' : 100 }
+
+COMPANIES = {
+        "Apple" : [
+            "Apple OR "
+            +"#Apple OR "
+            +"iPad OR "
+            +"iphone OR "
+            +"iPod OR "
+            +"apple watch OR "
+            +"mac OR "
+            +"macbook OR "
+            +"iMac OR "
+            ],
+        "Facebook" : [
+            "Facebook OR "
+            +"#Facebook OR "
+            +"news feed OR "
+            +"poke OR "
+            +"status OR "
+            +"timeline OR "
+            +"messenger OR "
+            ],
+        "Costa" : [
+            'Costa OR '
+            +'#Costa OR '
+            +'Whitbread OR '
+            ],
+        "Microsoft" : [
+            'Mircosoft OR '
+            +'#Mircosoft OR '
+            +'Surface OR '
+            +'power point OR '
+            +'excel OR '
+            +'vista OR '
+            +'kinect OR '
+            +'bing OR '
+            +'visual basic OR '
+            +'visual studio OR '
+            +'ms-dos OR '
+            ]
+}
+
 DATABASES = {'RAW_TWEETS_DB': 'raw_tweets.db',
              'FEATURES_DB'  : 'extracted_features.db',
              'STUB_RAW_TWEETS_DB': 'stub_raw_tweets.db'}
